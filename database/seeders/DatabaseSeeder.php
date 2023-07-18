cc79b268ca35af08bb7364a794c6ac8beeae5c11
rifat <rifat32>
2023-07-19 01:11:21 +0600

    first commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        if (!User::where('email', 'admin@app.com')->exists()) {
+            User::create([
+                'name' => 'Admin',
+                'email' => 'admin@app.com',
+                'email_verified_at' => Carbon::now(),
+                'password' => bcrypt('password'),
+                'role' => 'Admin'
+            ]);
+        }
+        $this->call(SettingSeeder::class);
+        // \App\Models\User::factory(10)->create();
+    }
+}
